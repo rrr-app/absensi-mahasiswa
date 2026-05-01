88b9e2448a347a9e3d170f8a78c0292b656a3d68
rrr-app <raudhatunnisa75@gmail.com>
2026-05-01 20:02:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,15 @@
 // config.js
 const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwufW0R8WsyPbW6hfvOamw7YEdTRd27Q8oh_pLFxinfhJuQ4UfJoLuHvaPvtFW4hzRp/exec';
+// File konfigurasi untuk aplikasi absensi
+const APP_CONFIG = {
+    appName: 'Aplikasi Absensi Mahasiswa',
+    version: '1.0.0',
+    prodi: 'Teknik Informatika',
+    mataKuliah: 'Pemrograman Web'
+};
 
+console.log('📋 Aplikasi Absensi Mahasiswa v1.0.0');
+console.log('Program Studi:', APP_CONFIG.prodi);
+console.log('Mata Kuliah:', APP_CONFIG.mataKuliah);
 console.log('🚀 Config loaded!');
 console.log('URL:', GOOGLE_SCRIPT_URL);
