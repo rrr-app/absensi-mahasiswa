f9232712f1e58819cf7800e1891153ced007f68b
rrr-app <raudhatunnisa75@gmail.com>
2026-05-01 19:45:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwufW0R8WsyPbW6hfvOamw7YEdTRd27Q8oh_pLFxinfhJuQ4UfJoLuHvaPvtFW4hzRp/exec';
 
 console.log('🚀 Config loaded!');
 console.log('URL:', GOOGLE_SCRIPT_URL);
